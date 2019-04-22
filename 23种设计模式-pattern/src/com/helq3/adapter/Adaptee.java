003303b57f0751c860f5b3984a2f35feeb83e170
Helena <2817570123@qq.com>
2019-04-22 22:35:57 +0800

    1

---
+++ 23种设计模式-pattern/src/com/helq3/adapter/Adaptee.java
@@ -0,0 +1,12 @@
+package com.helq3.adapter;
+/**
+ * 需要适配的类
+ * 相当于例子中的键盘
+ * @author Helena https://github.com/helqqq/pattern.git
+ *
+ */
+public class Adaptee {
+	public void request(){
+		System.out.println("可以完成客户需求的功能");
+	}
+}
